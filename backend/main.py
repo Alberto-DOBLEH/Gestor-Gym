a7ab066cce6fa097503d315c0c8546d2b39fbbce
Alberto Hernandez <luis.alberto.hdez.hdez245@gmail.com>
2025-06-09 22:19:40 -0700

    Ya jalan estos dos endpoints
    Endpoints:
    -Obtener info de los empleados
    -Obtener info de empleado por id

--- backend/main.py
+++ backend/main.py
@@ -4,17 +4,28 @@ app = Flask(__name__)
 
 
 # Enpoint Muestra todos los Usuarios
-@app.route("/empleados", methods=["GET"])
+@app.route("/empleados/", methods=["GET"])
 def get_empleados():
     resultados = []
     result = modelos_empleados.obtener_empleados()
-    claves = ['id', 'nombres', 'correo']
+    claves = ['id_empleado','nombre','primero_apellido','segundo_apellido','RFC','CURP','NSS','numero_telefono','tipo_empleado']
     for objetos in result:
         list2dic = dict(zip(claves, objetos)) #convertimos la tupla result y la lista claves en un diccionario
         resultados.append(list2dic)
 
-    return jsonify({"data":resultados})
+    return jsonify(resultados)
 
+# Endpoint para obtener la informacion del usuario por medio de la id
+@app.route("/empleados/<int:id>", methods=["GET"])
+def get_empleado_byid(id):
+    resultados = []
+    result = modelos_empleados.obtener_empleado_porid(id)
+    claves = ['id_empleado','nombre','primero_apellido','segundo_apellido','RFC','CURP','NSS','numero_telefono','tipo_empleado']
+    for objetos in result:
+        list2dic = dict(zip(claves, objetos))
+        resultados.append(list2dic)
+
+    return jsonify(resultados)
 
 
 if __name__ == '__main__':
--- backend/modelos/modelos_empleados.py
+++ backend/modelos/modelos_empleados.py
@@ -10,3 +10,14 @@ def obtener_empleados():
     cnx.close()
 
     return data
+
+def obtener_empleado_porid(id):
+    cnx = conexion()
+    cursor = cnx.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id_empleado = %s", id)
+    data = cursor.fetchall()
+
+    cursor.close()
+    cnx.close()
+
+    return data
